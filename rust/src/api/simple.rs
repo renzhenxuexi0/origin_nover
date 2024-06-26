42003ff13484711dc0fa07178ea1fbb7de9ff68d
wuhaiyue <wuhaiyue@leyaoyao.com>
2024-06-26 20:13:50 +0800

    feat(json): 新增json解析

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -24,3 +24,31 @@ pub fn init_app() {
     // Default utilities - feel free to customize
     flutter_rust_bridge::setup_default_user_utils();
 }
+
+
+#[cfg(test)]
+mod tests {
+    use serde::{Deserialize, Serialize};
+    use serde_json::Value;
+
+    #[derive(Serialize, Deserialize, Debug)]
+    struct Url {
+        title: String,
+        url: String,
+        style: String,
+    }
+
+    #[test]
+    fn test_parse_json() {
+        // 读取文件 assets/book_store.json
+        let json = std::fs::read_to_string("D:/flutter_project/origin_novel/rust/assets/new.json").unwrap().to_string();
+        let books: Value = serde_json::from_str(&json).unwrap();
+        let explore = books.get("exploreUrl").unwrap().to_string();
+        let result_string = explore.replace("\\\"", "\"").replace("\\n", "").replace("\\\\", "\\");
+        let result = &result_string[2..(result_string.len() - 2)];
+
+
+        // println!("books = {:#?}", serde_json::from_str::<Value>(result).unwrap());
+        println!("books = {:#?}", result);
+    }
+}
